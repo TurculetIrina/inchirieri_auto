e31b74d8a84c7616ab124226db99093f5132840e
loghin sebastian <tyli698@gmail.com>
2020-04-03 17:13:51 +0300

    Add project files.

---
+++ inchirieri-auto/Masini.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inchirieri_auto
+{
+    class Masini
+    {
+        public string Marca { get; set; }
+        public string Model { get; set; }
+        public string NumarDeInmatriculare { get; set; }
+        public int AnFabricatie { get; set; }
+        public int CapacitateMotor { get; set; }
+        public bool Inchiriata {get; set;}
+
+        public Masini()
+        {
+            Marca = Model = NumarDeInmatriculare = string.Empty;
+            AnFabricatie = CapacitateMotor = 0;
+            Inchiriata = false;
+        }
+
+        public Masini(string _marca, string _model, string _numar, 
+            int _an_fabricatie, int _capacitate_motor, bool _inchiriata)
+        {
+            Marca = _marca;
+            Model = _model;
+            NumarDeInmatriculare = _numar;
+            AnFabricatie = _an_fabricatie;
+            CapacitateMotor = _capacitate_motor;
+            Inchiriata = _inchiriata;
+        }
+
+        public string ConversieLaSir()
+        {
+            return string.Format("{0}, {1}, {2}, {3}, {4}",
+                Marca, Model, NumarDeInmatriculare, AnFabricatie, CapacitateMotor);
+        }
+
+        public bool Compare(Masini masina)
+        {
+            if (NumarDeInmatriculare == masina.NumarDeInmatriculare)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ inchirieri-auto/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inchirieri_auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test clasa Masini
+            var masina = new Masini("Audi", "RS7", "SV01IRI", 2019, 3500, false);
+            Console.WriteLine(masina.ConversieLaSir());
+            var masina2 = new Masini("Audi", "R8", "SV02IRI", 2019, 4800, false);
+            Console.WriteLine(masina2.ConversieLaSir());
+            if (masina.Compare(masina2))
+                Console.WriteLine("Masina este aceeasi");
+            else
+                Console.WriteLine("Masina NU este aceeasi");
+            Console.ReadKey();
+
+            // Test clasa Angajati
+            var angajat = new Angajati("Turculet", "Irina", "Cuza Voda", "0745454545", 22,
+                "2971231565658", "Vanzari", 4, 3, 2020);
+            Console.WriteLine(angajat.ConversieLaSir());
+            var angajat2 = new Angajati("Turculet", "Irina", "Cuza Voda", "0745454545", 22,
+                "2971231565658", "Vanzari", 4, 3, 2020);
+            Console.WriteLine(angajat2.ConversieLaSir());
+            if (angajat.Compare(angajat2))
+                Console.WriteLine("Este acelasi angajat");
+            else
+                Console.WriteLine("NU este acelasi angajat");
+            Console.ReadKey();
+
+            // Test clasa Clienti
+            var client = new Clienti("Turculet", "Georgel", "Grigore Antipa", "0741414141", 27, "1921122565859",
+                "SV92TGM", 1, 2, 2020, 7);
+            Console.WriteLine(client.ConversieLaSir());
+            Console.ReadKey();
+        }
+    }
+}
